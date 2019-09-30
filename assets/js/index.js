9184d98e838f80cd818755df064eb5c7a6e83ab8
Michael Jacquemart <michaeljacquemart.webdev@gmail.com>
2019-09-30 16:22:00 +0200

    Parcel

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 "use strict";
-var a = 1;
-var b = 4;
-var c = a + b;
-console.log(c);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var message_1 = __importDefault(require("./message"));
+console.log(message_1.default);
--- assets/ts/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var a = 1;
-var b = 4;
-var c = a + b;
-console.log(c);
